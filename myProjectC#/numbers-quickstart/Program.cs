8579389ed2ead53fcb5648f90d752be2861e2d3d
Mason <donnma01@luther.edu>
2018-09-17 22:03:55 -0500

    Tried Lists and Numbers in C#

---
+++ myProjectC#/numbers-quickstart/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            //WorkingWithIntegers();
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+            double max2 = double.MaxValue;
+            double min2 = double.MinValue;
+            Console.WriteLine($"The range of double is {min2} to {max2}");
+        }
+    }
+}
