3426e3c8ca0d3adde6df1943ab6fe7800dc77e80
conzty01 <ty54@sbcglobal.net>
2018-10-05 11:56:05 -0500

    Combined current Thomas's database settings with Mason's server

--- server/Startup.cs
+++ server/Startup.cs
@@ -8,16 +8,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Data.SqlClient;
 using System.Text;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
 
 namespace server
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -27,48 +48,58 @@ namespace server
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
-            app.Run(async (context) =>
-            {                
-                string first_name;
-                //string last_name;
-                //string genre;
-                //string instrument;
-                //string message;
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "pluggedindb.database.windows.net";
-                builder.UserID = "sqladmin";
-                builder.Password = "$had0wrunBlu3";
-                builder.InitialCatalog = "pluggedin_db";
+            app.UseMvc();
+             
+            string first_name;
+            //string last_name;
+            //string genre;
+            //string instrument;
+            //string message;
 
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "pluggedindb.database.windows.net";
+            builder.UserID = "sqladmin";
+            builder.Password = "$had0wrunBlu3";
+            builder.InitialCatalog = "pluggedin_db";
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                StringBuilder sb = new StringBuilder();
+                sb.Append("SELECT first_name ");
+                sb.Append("FROM musicians ");
+                String sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
                 {
-                    connection.Open();
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append("SELECT first_name ");
-                    sb.Append("FROM musicians ");
-                    String sql = sb.ToString();
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        while (reader.Read())
                         {
-                            while (reader.Read())
-                            {
-                                first_name = reader.GetString(0);
-                                //last_name = reader.GetString(1);
-                                //genre = reader.GetString(2);
-                                //instrument = reader.GetString(3);
+                            first_name = reader.GetString(0);
+                            //last_name = reader.GetString(1);
+                            //genre = reader.GetString(2);
+                            //instrument = reader.GetString(3);
 
-                                //message = first_name + last_name + " a " + genre + " musician plays the " + instrument;
-                                await context.Response.WriteAsync("Hello World! \n" + first_name);
-                            }
+                            //message = first_name + last_name + " a " + genre + " musician plays the " + instrument;
+                            Console.WriteLine(first_name);
+                            //The above writes to the App stdout.
+
+                            //await context.Response.WriteAsync("Hello World! \n" + message);
                         }
                     }
                 }
-
-            });
+            }
         }
     }
 }
