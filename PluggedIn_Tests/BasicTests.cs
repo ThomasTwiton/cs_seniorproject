c8fdc1528e783945184e56e51a86382b4fb89aba
conzty01 <ty54@sbcglobal.net>
2018-10-12 18:16:03 -0500

    Created xUnit tests and attached them to the server solution. In order to run the tests, simply go to Test > Run > All Tests. Currently, the only tests that exist are simple tests to confirm that we haven't broken the basic functionality of C# with our changes.

---
+++ PluggedIn_Tests/BasicTests.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+
+namespace PluggedIn_Tests
+{
+    public class CSharpTests
+    {
+        // Facts are tests should be used when you expect 
+        //  the same result from the test no matter the input.
+        [Fact]
+        public void PassingTest()
+        {
+            // The common practice for unit tests is to divide 
+            //  the test into 3 parts: Arrange, Act, and Assert.
+
+            //  1.) Arrange all necessary preconditions and inputs.
+            //  2.) Act on the object or method under test.
+            //  3.) Assert that the expected results have occurred.
+
+            // Arrange
+            int num = 2;
+            int sum;
+
+            // Act
+            sum = num + num;
+
+            // Assert
+            Assert.Equal(4, sum);
+        }
+
+
+        // Theories want to test the same method with many 
+        //  different inputs to confirm that it holds up.
+
+        // Data is passed into the exam below using the 
+        //  [InlineData] attribute.
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void MyFirstTheory(int value)
+        {
+            // Arrange
+            bool isOdd;
+
+            // Act
+            isOdd = value % 2 == 1;
+
+            // Assert
+            Assert.True(isOdd);
+        }
+
+    }
+}
