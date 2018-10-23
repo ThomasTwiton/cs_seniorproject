4157e8b681c3d20b369d38a501e4bada7287281d
Thomas Twiton <twitth01@luther.edu>
2018-10-22 21:51:30 -0500

    Most of our full database, using Entity MVC

---
+++ pluggedIn_entity/pluggedIn_entity/Controllers/ProfilesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using pluggedIn_entity.Models;
+
+namespace pluggedIn_entity.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        private readonly PluggedContext _context;
+
+        public ProfilesController(PluggedContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Profiles
+        public async Task<IActionResult> Index()
+        {
+            var pluggedContext = _context.Profiles.Include(p => p.User);
+            return View(await pluggedContext.ToListAsync());
+        }
+
+        // GET: Profiles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profile = await _context.Profiles
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.ProfileId == id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return View(profile);
+        }
+
+        // GET: Profiles/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Profiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProfileId,First_Name,Last_Name,Preferred_Name,Pic_Url,UserId")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(profile);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", profile.UserId);
+            return View(profile);
+        }
+
+        // GET: Profiles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profile = await _context.Profiles.FindAsync(id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", profile.UserId);
+            return View(profile);
+        }
+
+        // POST: Profiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProfileId,First_Name,Last_Name,Preferred_Name,Pic_Url,UserId")] Profile profile)
+        {
+            if (id != profile.ProfileId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(profile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfileExists(profile.ProfileId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", profile.UserId);
+            return View(profile);
+        }
+
+        // GET: Profiles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profile = await _context.Profiles
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.ProfileId == id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return View(profile);
+        }
+
+        // POST: Profiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var profile = await _context.Profiles.FindAsync(id);
+            _context.Profiles.Remove(profile);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfileExists(int id)
+        {
+            return _context.Profiles.Any(e => e.ProfileId == id);
+        }
+    }
+}
---
+++ pluggedIn_entity/pluggedIn_entity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pluggedIn_entity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
