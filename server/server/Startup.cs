81633235dde26361e91692a5f535b3e1dab6c4e3
Mason Donnohue <donnma01@luther.edu>
2018-10-26 16:26:26 -0500

    New files

--- server/server/Startup.cs
+++ server/server/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using server.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -33,6 +35,9 @@ namespace server
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=server;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<PluggedContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
