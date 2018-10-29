d268bd33898171882202a6e359830c518f99b905
Thomas Twiton <twitth01@luther.edu>
2018-10-28 19:51:46 -0500

    All database tables

---
+++ server/server/Migrations/20181029003425_InitialCreateFull.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class InitialCreateFull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Instruments",
+                columns: table => new
+                {
+                    InstrumentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Instrument_Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instruments", x => x.InstrumentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostMedias",
+                columns: table => new
+                {
+                    PostMediaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MediaUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostMedias", x => x.PostMediaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ensembles",
+                columns: table => new
+                {
+                    EnsembleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Ensemble_Name = table.Column<string>(nullable: true),
+                    Formed_Date = table.Column<DateTime>(nullable: false),
+                    Disbanded_Date = table.Column<DateTime>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Genre = table.Column<string>(nullable: true),
+                    Pic_Url = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ensembles", x => x.EnsembleId);
+                    table.ForeignKey(
+                        name: "FK_Ensembles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    ProfileId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    First_Name = table.Column<string>(nullable: true),
+                    Last_Name = table.Column<string>(nullable: true),
+                    Preferred_Name = table.Column<string>(nullable: true),
+                    Pic_Url = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.ProfileId);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venues",
+                columns: table => new
+                {
+                    VenueId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Venue_Name = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venues", x => x.VenueId);
+                    table.ForeignKey(
+                        name: "FK_Venues_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Auditoins",
+                columns: table => new
+                {
+                    AuditionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Open_Date = table.Column<DateTime>(nullable: false),
+                    Closed_Date = table.Column<DateTime>(nullable: false),
+                    Audition_Location = table.Column<string>(nullable: true),
+                    Audition_Description = table.Column<string>(nullable: true),
+                    Instrument_Name = table.Column<string>(nullable: true),
+                    EnsembleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Auditoins", x => x.AuditionId);
+                    table.ForeignKey(
+                        name: "FK_Auditoins_Ensembles_EnsembleId",
+                        column: x => x.EnsembleId,
+                        principalTable: "Ensembles",
+                        principalColumn: "EnsembleId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "E_Has_Medias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProfileId = table.Column<int>(nullable: false),
+                    MediaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_E_Has_Medias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_E_Has_Medias_PostMedias_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "PostMedias",
+                        principalColumn: "PostMediaId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_E_Has_Medias_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "ProfileId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "P_Has_Medias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProfileId = table.Column<int>(nullable: false),
+                    MediaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_P_Has_Medias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_P_Has_Medias_PostMedias_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "PostMedias",
+                        principalColumn: "PostMediaId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_P_Has_Medias_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "ProfileId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plays_Instruments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProfileId = table.Column<int>(nullable: false),
+                    InstrumentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plays_Instruments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Plays_Instruments_Instruments_InstrumentId",
+                        column: x => x.InstrumentId,
+                        principalTable: "Instruments",
+                        principalColumn: "InstrumentId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Plays_Instruments_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "ProfileId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProfileEnsembles",
+                columns: table => new
+                {
+                    Start_Date = table.Column<DateTime>(nullable: false),
+                    End_Date = table.Column<DateTime>(nullable: false),
+                    ProfileId = table.Column<int>(nullable: false),
+                    EnsembleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfileEnsembles", x => new { x.ProfileId, x.EnsembleId });
+                    table.ForeignKey(
+                        name: "FK_ProfileEnsembles_Ensembles_EnsembleId",
+                        column: x => x.EnsembleId,
+                        principalTable: "Ensembles",
+                        principalColumn: "EnsembleId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ProfileEnsembles_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "ProfileId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "V_Has_Medias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProfileId = table.Column<int>(nullable: false),
+                    MediaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_V_Has_Medias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_V_Has_Medias_PostMedias_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "PostMedias",
+                        principalColumn: "PostMediaId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_V_Has_Medias_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "ProfileId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gigs",
+                columns: table => new
+                {
+                    GigId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Gig_Date = table.Column<DateTime>(nullable: false),
+                    VenueId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gigs", x => x.GigId);
+                    table.ForeignKey(
+                        name: "FK_Gigs_Venues_VenueId",
+                        column: x => x.VenueId,
+                        principalTable: "Venues",
+                        principalColumn: "VenueId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Booked_Gigs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date_Booked = table.Column<DateTime>(nullable: false),
+                    GigId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Booked_Gigs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Booked_Gigs_Gigs_GigId",
+                        column: x => x.GigId,
+                        principalTable: "Gigs",
+                        principalColumn: "GigId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Auditoins_EnsembleId",
+                table: "Auditoins",
+                column: "EnsembleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Booked_Gigs_GigId",
+                table: "Booked_Gigs",
+                column: "GigId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_E_Has_Medias_MediaId",
+                table: "E_Has_Medias",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_E_Has_Medias_ProfileId",
+                table: "E_Has_Medias",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ensembles_UserId",
+                table: "Ensembles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gigs_VenueId",
+                table: "Gigs",
+                column: "VenueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_P_Has_Medias_MediaId",
+                table: "P_Has_Medias",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_P_Has_Medias_ProfileId",
+                table: "P_Has_Medias",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plays_Instruments_InstrumentId",
+                table: "Plays_Instruments",
+                column: "InstrumentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plays_Instruments_ProfileId",
+                table: "Plays_Instruments",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileEnsembles_EnsembleId",
+                table: "ProfileEnsembles",
+                column: "EnsembleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_V_Has_Medias_MediaId",
+                table: "V_Has_Medias",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_V_Has_Medias_ProfileId",
+                table: "V_Has_Medias",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venues_UserId",
+                table: "Venues",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Auditoins");
+
+            migrationBuilder.DropTable(
+                name: "Booked_Gigs");
+
+            migrationBuilder.DropTable(
+                name: "E_Has_Medias");
+
+            migrationBuilder.DropTable(
+                name: "P_Has_Medias");
+
+            migrationBuilder.DropTable(
+                name: "Plays_Instruments");
+
+            migrationBuilder.DropTable(
+                name: "ProfileEnsembles");
+
+            migrationBuilder.DropTable(
+                name: "V_Has_Medias");
+
+            migrationBuilder.DropTable(
+                name: "Gigs");
+
+            migrationBuilder.DropTable(
+                name: "Instruments");
+
+            migrationBuilder.DropTable(
+                name: "Ensembles");
+
+            migrationBuilder.DropTable(
+                name: "PostMedias");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+
+            migrationBuilder.DropTable(
+                name: "Venues");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ server/server/Models/Tables.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace server.Models
+{
+    public class PluggedContext : DbContext
+    {
+
+        public PluggedContext(DbContextOptions<PluggedContext> options)
+            : base(options)
+        {  }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProfileEnsemble>()
+                .HasKey(em => new { em.ProfileId, em.EnsembleId });
+
+            modelBuilder.Entity<ProfileEnsemble>()
+                .HasOne(em => em.Profile)
+                .WithMany(b => b.ProfileEnsemble)
+                .HasForeignKey(em => em.ProfileId);
+
+            modelBuilder.Entity<ProfileEnsemble>()
+                .HasOne(em => em.Ensemble)
+                .WithMany(c => c.ProfileEnsemble)
+                .HasForeignKey(em => em.EnsembleId);
+
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                // equivalent of modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+                // and modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+                entityType.GetForeignKeys()
+                    .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade)
+                    .ToList()
+                    .ForEach(fk => fk.DeleteBehavior = DeleteBehavior.Restrict);
+            }
+
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Ensemble> Ensembles { get; set; }
+        public DbSet<Instrument> Instruments{ get; set; }
+        public DbSet<Plays_Instrument> Plays_Instruments { get; set; }
+        public DbSet<ProfileEnsemble> ProfileEnsembles { get; set; }
+        public DbSet<Audition> Auditoins { get; set; }
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Booked_Gig> Booked_Gigs { get; set; }
+        public DbSet<PostMedia> PostMedias { get; set; }
+        public DbSet<E_Has_Media> E_Has_Medias { get; set; }
+        public DbSet<P_Has_Media> P_Has_Medias { get; set; }
+        public DbSet<V_Has_Media> V_Has_Medias { get; set; }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Profile> Profile { get; set; }
+        public ICollection<Ensemble> Ensemble { get; set; }
+        public ICollection<Venue> Venue { get; set; }
+    }
+
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Preferred_Name { get; set; }
+        public string Pic_Url { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Plays_Instrument> Plays_Instrument { get; set; }
+        public ICollection<ProfileEnsemble> ProfileEnsemble { get; set; }
+    }
+
+    public class Ensemble
+    {
+        public int EnsembleId { get; set; }
+
+        public string Ensemble_Name { get; set; }
+        public System.DateTime Formed_Date { get; set; }
+        public System.DateTime Disbanded_Date { get; set; }
+        public string Type { get; set; }
+        public string Genre { get; set; }
+        public string Pic_Url { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<ProfileEnsemble> ProfileEnsemble { get; set; }
+        public ICollection<Audition> Audition { get; set; }
+    }
+
+    public class Venue
+    {
+        public int VenueId { get; set; }
+
+        public string Venue_Name { get; set; }
+        public string Location { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Gig> Gig { get; set; }
+    }
+
+    public class Instrument
+    {
+        public int InstrumentId { get; set; }
+        public string Instrument_Name { get; set; }
+
+        public ICollection<Plays_Instrument> Plays_Instrument { get; set; }
+    }
+
+    public class Plays_Instrument
+    {
+        public int Id { get; set; }
+
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+
+        public int InstrumentId { get; set; }
+        public Instrument Instrument { get; set; }
+    }
+ 
+    public class ProfileEnsemble
+    {
+        public System.DateTime Start_Date { get; set; }
+        public System.DateTime End_Date { get; set; }
+
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+        
+        public int EnsembleId { get; set; }
+        public Ensemble Ensemble { get; set; }
+    }
+  
+
+    public class Audition
+    {
+        public int AuditionId { get; set; }
+        public System.DateTime Open_Date { get; set; }
+        public System.DateTime Closed_Date { get; set; }
+        public string Audition_Location { get; set; }
+        public string Audition_Description { get; set; }
+        public string Instrument_Name { get; set; }
+
+        public int EnsembleId { get; set; }
+        public Ensemble Ensemble { get; set; }
+    }
+
+    public class Gig
+    {
+        public int GigId { get; set; }
+        public System.DateTime Gig_Date { get; set; }
+
+        public int VenueId { get; set; }
+        public Venue Venue { get; set; }
+
+        public ICollection<Booked_Gig> Booked_Gig { get; set; }
+    }
+
+    public class Booked_Gig
+    {
+        public int Id { get; set; }
+        public System.DateTime Date_Booked { get; set; }
+
+        public int GigId { get; set; }
+        public Gig Gig { get; set; }
+
+    }
+
+    public class PostMedia
+    {
+        public int PostMediaId { get; set; }
+        public string MediaUrl { get; set; }
+
+        public ICollection<P_Has_Media> P_Has_Media { get; set; }
+        public ICollection<E_Has_Media> E_Has_Media { get; set; }
+        public ICollection<V_Has_Media> V_Has_Media { get; set; }
+    }
+
+    public class P_Has_Media
+    {
+        public int Id { get; set; }
+
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+
+        public int MediaId { get; set; }
+        public PostMedia Media { get; set; }
+    }
+
+    public class E_Has_Media
+    {
+        public int Id { get; set; }
+
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+
+        public int MediaId { get; set; }
+        public PostMedia Media { get; set; }
+    }
+
+    public class V_Has_Media
+    {
+        public int Id { get; set; }
+
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+
+        public int MediaId { get; set; }
+        public PostMedia Media { get; set; }
+    }
+
+    
+
+}
