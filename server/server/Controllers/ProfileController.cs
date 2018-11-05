c021af28f20fbc7aec230ece5c97bdb14664fc35
conzty01 <ty54@sbcglobal.net>
2018-11-04 21:38:50 -0600

    Created profile/ensemble page and built React components for displaying the posts on a page. Also updated some misc site-wide css stuff.

---
+++ server/server/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
+
+namespace server.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Ringo Starr - Profile";
+            ViewData["Bio"] = "English musician, singer, actor, songwriter, and drummer for the Beatles.";
+            ViewData["Location"] = "Liverpool";
+            ViewData["ProfPicURL"] = "https://placekitten.com/g/64/64";
+            ViewData["Name"] = "Ringo Starr";
+            ViewData["Posts"] = new List<object>();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
