fbd8f1a29d14f3866521a89cb1fdd3b756ea4364
conzty01 <ty54@sbcglobal.net>
2018-11-08 14:32:51 -0600

    Further updates for the UI

--- server/server/Controllers/ProfileController.cs
+++ server/server/Controllers/ProfileController.cs
@@ -18,6 +18,18 @@ namespace server.Controllers
             ViewData["ProfPicURL"] = "https://placekitten.com/g/64/64";
             ViewData["Name"] = "Ringo Starr";
             ViewData["Posts"] = new List<object>();
+            ViewData["Owner"] = "false";
+            return View();
+        }
+
+        public IActionResult Venue()
+        {
+            ViewData["Title"] = "Ringo Starr - Profile";
+            ViewData["Bio"] = "English musician, singer, actor, songwriter, and drummer for the Beatles.";
+            ViewData["Location"] = "Liverpool";
+            ViewData["ProfPicURL"] = "https://placekitten.com/g/64/64";
+            ViewData["Name"] = "Ringo Starr";
+            ViewData["Posts"] = new List<object>();
             return View();
         }
 
---
+++ server/server/wwwroot/js/ensemble.js
@@ -0,0 +1,41 @@
+﻿function EnsembleContainer(props) {
+    // The container that holds all the members inside of it.
+
+    const membersList = props.members;
+    const memberItems = membersList.map((m) => <Member info={m} />);
+    return (
+        <div id="ensembleContainer" className="container">
+            {memberItems}
+        </div>
+    );
+}
+
+function MemberRow(props) {
+    // The rows in the EnsembleContainer that hold the Members.
+}
+
+function Member(props) {
+    // The base component for a member.
+    return (
+        <div)
+}
+
+const e = {
+    name: 'The Beatles',
+    avatarURL: 'https://placekitten.com/g/64/64',
+    profileID: 1,
+}
+
+const n = {
+    name: 'Solo',
+    avatarURL: 'https://placekitten.com/g/64/64',
+    profileID: null,
+}
+
+const eList = [e, n];
+console.log(eList);
+
+ReactDOM.render(
+    <EnsembleContainer members={eList} />,
+    document.getElementById('ensembles')
+);
