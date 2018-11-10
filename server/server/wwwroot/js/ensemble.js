219cf2aadcee0022f8aead9d323a4778a49ff6de
conzty01 <ty54@sbcglobal.net>
2018-11-09 21:21:38 -0600

    Created an extra button in the ensemble React component

--- server/server/wwwroot/js/ensemble.js
+++ server/server/wwwroot/js/ensemble.js
@@ -1,4 +1,8 @@
-﻿function EnsembleContainer(props) {
+﻿/* ================================================== */
+/* ================ React Components ================ */
+/* ================================================== */
+
+function EnsembleContainer(props) {
     // The container that holds all the members inside of it.
 
     const membersList = props.members;
@@ -6,19 +10,54 @@
     return (
         <div id="ensembleContainer" className="container-fluid">
             {memberItems}
+            <ExtraOption />
         </div>
     );
 }
 
 function Member(props) {
     // The base component for a member.
-    console.log(props);
+
     const link = "/Profile/id=" + props.info.profileID.toString();
     return (
-        <a class="ensembleLink" href={link}>
+        <a className="ensembleLink" href={link}>
             <img src={props.info.avatarURL} title={props.info.name} />
         </a>
     );
+}
+
+function ExtraOption(props) {
+    // An additional square that is used to create an audition, 
+    // create an ensemble or other such functionality.
+
+    return (
+        <div className="extraBit" onClick={() => { showModal() }}>
+            <span className="glyphicon glyphicon-plus"></span>
+        </div>
+    );
+}
+
+/* ================================================== */
+/* ================= Modal Functions ================ */
+/* ================================================== */
+
+function showModal() {
+    // Get the modal
+    let modal = document.getElementById('myModal');
+    modal.style.display = "block";
+}
+
+function hideModal() {
+    let modal = document.getElementById('myModal');
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    let modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 const e = {
@@ -26,31 +65,26 @@ const e = {
     avatarURL: 'https://placekitten.com/g/64/64',
     profileID: 1,
 }
-
 const n = {
     name: 'Solo',
     avatarURL: 'https://placekitten.com/g/64/64',
     profileID: 2,
 }
-
 const s = {
     name: "Jeremy's Band",
     avatarURL: 'https://placekitten.com/g/64/64',
     profileID: 3,
 }
-
 const m = {
     name: 'Luther College Jazz Orchestra',
     avatarURL: 'https://placekitten.com/g/64/64',
     profileID: 4,
 }
-
 const b = {
     name: "Peter's Jazz Combo",
     avatarURL: 'https://placekitten.com/g/64/64',
     profileID: 5,
 }
-
 const l = {
     name: 'Gandolf the Hobbits',
     avatarURL: 'https://placekitten.com/g/64/64',
