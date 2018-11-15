01a26a7cc522495af4bf3509d964989b0b273a26
conzty01 <ty54@sbcglobal.net>
2018-11-14 20:54:37 -0600

    Began formatting ui to fit to mason's controllers

--- server/server/wwwroot/js/ensemble.js
+++ server/server/wwwroot/js/ensemble.js
@@ -1,8 +1,4 @@
-﻿/* ================================================== */
-/* ================ React Components ================ */
-/* ================================================== */
-
-function EnsembleContainer(props) {
+﻿function EnsembleContainer(props) {
     // The container that holds all the members inside of it.
 
     const membersList = props.members;
@@ -10,15 +6,14 @@ function EnsembleContainer(props) {
     return (
         <div id="ensembleContainer" className="container-fluid">
             {memberItems}
-            <ExtraOption />
         </div>
     );
 }
 
 function Member(props) {
     // The base component for a member.
-
-    const link = "/Profile/id=" + props.info.profileID.toString();
+    //console.log(props);
+    const link = "ViewEnsemble/" + props.info.id.toString();
     return (
         <a className="ensembleLink" href={link}>
             <img src={props.info.avatarURL} title={props.info.name} />
@@ -26,65 +21,30 @@ function Member(props) {
     );
 }
 
-function ExtraOption(props) {
-    // An additional square that is used to create an audition, 
-    // create an ensemble or other such functionality via modal.
+function pageLoaded() {
+    /* The way that the ensembles data is being passed into
+     * the view requires the javascript to find all of the 
+     * member tags and assemble the appropriate props to be
+     * passed to the EnsembleContainer. */
 
-    let modal = document.getElementById('createModal');
-    if (modal == null) {
-        return (null)
+    let eList = document.getElementsByTagName("Member");
+    //console.log(eList);
+    let propList = [];
+    for (let m of eList) {
+        let props = {}
+        props.id = m.dataset.id;
+        props.avatarURL = m.dataset.avatarURL;
+        props.name = m.dataset.name;
+
+        propList.push(props)
     }
-
-    let message;
-    console.log(profileType);
-    if (profileType == "ensemble") {
-        message = "Hold Auditions!";
-    } else {
-        message = "Form a Group!";
-    }
-
-    return (
-        <div title={message} className="extraBit" onClick={() => { showModal() }}>
-            <span className="glyphicon glyphicon-plus"></span>
-        </div>
-    );
+    //console.log(propList);
+    return propList;
 }
 
-const e = {
-    name: 'The Beatles',
-    avatarURL: 'https://placekitten.com/g/64/64',
-    profileID: 1,
-}
-const n = {
-    name: 'Solo',
-    avatarURL: 'https://placekitten.com/g/64/64',
-    profileID: 2,
-}
-const s = {
-    name: "Jeremy's Band",
-    avatarURL: 'https://placekitten.com/g/64/64',
-    profileID: 3,
-}
-const m = {
-    name: 'Luther College Jazz Orchestra',
-    avatarURL: 'https://placekitten.com/g/64/64',
-    profileID: 4,
-}
-const b = {
-    name: "Peter's Jazz Combo",
-    avatarURL: 'https://placekitten.com/g/64/64',
-    profileID: 5,
-}
-const l = {
-    name: 'Gandolf the Hobbits',
-    avatarURL: 'https://placekitten.com/g/64/64',
-    profileID: 6,
-}
-
-const eList = [e,n,s,m,b,l];
-console.log(eList);
-
+const propList = pageLoaded();
+console.log(propList);
 ReactDOM.render(
-    <EnsembleContainer members={eList} />,
+    <EnsembleContainer members={propList} />,
     document.getElementById('ensembles')
 );
