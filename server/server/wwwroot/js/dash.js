ed4f56dea75046c062a9ff087dfc97bd31553d5f
conzty01 <ty54@sbcglobal.net>
2019-03-01 18:08:02 -0600

    Edit and Delete buttons interact with javascript and display modal

--- server/server/wwwroot/js/dash.js
+++ server/server/wwwroot/js/dash.js
@@ -1,38 +1,14 @@
-﻿const { Tabs, TabList, Tab, TabPanel } = ReactTabs;
+﻿function editAud(id) {
+    // get info from the api
 
-function TabContainer(props) {
-    // The container that holds all posts inside of it.
+    // with that info, populate the modal
 
-    let tabList = [];
-    for (let i = 0; i < props.tabs.length; i++) {
-        tabList.push(<Tab>{props.title}</Tab>);
-    }
-
-    return (
-        <Tabs>
-            <TabList>
-                {tabList}
-            </TabList>
-
-            <TabPanel>
-                <h2>Any content 1</h2>
-            </TabPanel>
-            <TabPanel>
-                <h2>Any content 2</h2>
-            </TabPanel>
-        </Tabs>
-    );
+    // display modal
+    showModal();
+    console.log(id, _EnsembleId);
 }
 
-const t1 = { title: "Previous" };
-
-const t2 = { title: "Pending" };
-
-const t3 = { title: "Manage" };
-
-const tabList = [t1, t2, t3];
-
-ReactDOM.render(
-    <TabContainer tabs={tabList} />,
-    document.getElementById('tabCC')
-);
+function delAud(id) {
+    alert("Are you sure you would like to close this audition?\n\nThe audition can be reinstated in the 'Previous Auditions' tab.")
+    console.log(id, _EnsembleId);
+}
