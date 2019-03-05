1feedcf28e275ad733cc4bcdb469faa8187e11ab
conzty01 <ty54@sbcglobal.net>
2019-03-05 13:21:27 -0600

    Futher development

--- server/server/wwwroot/js/dash.js
+++ server/server/wwwroot/js/dash.js
@@ -1,4 +1,5 @@
 ﻿function editAud(id) {
+    console.log("Editing Audition:", id);
     // get info from the api
 
     // with that info, populate the modal
@@ -9,6 +10,11 @@
 }
 
 function delAud(id) {
+    console.log("Deleting Audition:", id);
     alert("Are you sure you would like to close this audition?\n\nThe audition can be reinstated in the 'Previous Auditions' tab.")
     console.log(id, _EnsembleId);
+}
+
+function delMem(id) {
+    console.log("Deleting Member:", id);
 }
