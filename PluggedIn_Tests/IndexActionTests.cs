26911e4d22a6ee610e1faf11a41cfc3aaa2f4df0
conzty01 <ty54@sbcglobal.net>
2019-04-11 13:50:05 -0500

    Resolved issue with unit testing with our login system.

--- PluggedIn_Tests/IndexActionTests.cs
+++ PluggedIn_Tests/IndexActionTests.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+using System.Collections;
 
 namespace PluggedIn_Tests
 {
@@ -49,13 +50,16 @@ namespace PluggedIn_Tests
         {
             /* Arrange */
 
+            // Set desired UserId
+            var activeUserId = 1;
 
             // Create a Mocked Profile table
             var pData = new List<Profile>
             {
-                new Profile { ProfileId = 11, First_Name = "Elijas", Last_Name = "Reshmi", UserId = 1 },
-                new Profile { ProfileId = 12, First_Name = "Eugenia", Last_Name = "Cornelius", UserId = 2 }
+                new Profile { ProfileId = 11, First_Name = "Elijas", Last_Name = "Reshmi", UserId = activeUserId },
+                new Profile { ProfileId = 12, First_Name = "Eugenia", Last_Name = "Cornelius", UserId = activeUserId + 1 }
             }.AsQueryable();
+
 
             // Create a Mocked DB set
             var mockProfiles = new Mock<DbSet<Profile>>();
@@ -74,23 +78,45 @@ namespace PluggedIn_Tests
             // Create a Mocked IHostingEnviornment
             var mockHostEnv = new Mock<IHostingEnvironment>();
 
-            // Create a Mocked IRequestCookieCollection with an empty cookie value
-            var cookieCollection = new Mock<IRequestCookieCollection>();
-            cookieCollection.SetupGet(c => c[It.IsAny<string>()]).Returns("11");
+            // Create a Mocked HomeController
+            /*  Q: Why are we mocking the thing we are testing?
+             *  A: We need to mock the Home Controller because of our login system
+             *      cannot realistically be tested using unit tests. Instead, it
+             *      would require integration tests. As a result, we mock our 
+             *      HomeController so that we can mock the GetSessionInfo() method. 
+             *      In doing this, we are able to specify whether a user is logged in
+             *      and who they are. One very important thing to include in any test
+             *      that has log in behavior, is the following line:
+             *      
+             *      controllerMock.CallBase = true;
+             *      
+             *      This line tells Moq that if a mocked method/action has not been
+             *      specified, then it should call the method on an actual instance
+             *      of HomeController. In doing so, we can test the appropriate 
+             *      methods/actions while mocking the login system.
+             */
+            var controllerMock = new Mock<HomeController>(mockDB.Object, mockHostEnv.Object);
 
-            var controller = new HomeController(LoadedContext, mockHostEnv.Object);
+            // Mock the request object and the resulting login information
+            SessionModel fakeSM = new SessionModel();
+            fakeSM.IsLoggedIn = true;
+            fakeSM.UserID = activeUserId;
+
+            var controller = controllerMock.Object;
             controller.ControllerContext = new ControllerContext();
             controller.ControllerContext.HttpContext = new DefaultHttpContext();
-            controller.ControllerContext.HttpContext.Request.Cookies = cookieCollection.Object;
+            var mockReq = controller.ControllerContext.HttpContext.Request;
 
-            
+            controllerMock.Setup(x => x.GetSessionInfo(mockReq)).Returns(fakeSM);
+            controllerMock.CallBase = true;
+
 
             /* Act */
             var result = controller.Index();
 
             /* Assert */
             var viewResult = Assert.IsType<ViewResult>(result);
-            Assert.Equal("Profile",viewResult.ViewName);
+            Assert.Equal("Profile", viewResult.ViewName);
 
         }
 
